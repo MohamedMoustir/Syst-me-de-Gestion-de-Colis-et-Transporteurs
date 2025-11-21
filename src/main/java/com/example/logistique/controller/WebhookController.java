adf982d42217a9b05f0312ac1f0d00e55283bd49
MohamedMoustir <itsmoustir@gmail.com>
2025-11-21 10:18:44 +0100

    SDGCET-64 CI/CD et Docker

---
+++ src/main/java/com/example/logistique/controller/WebhookController.java
@@ -0,0 +1,17 @@
+package com.example.logistique.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+
+    @PostMapping("/github-webhook")
+    public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
+        System.out.println("\n===== WEBHOOK RECEIVED =====");
+        System.out.println(payload);
+        return ResponseEntity.ok("OK");
+    }
+}
