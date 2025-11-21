c434abe0025236c35d28742eb113c589dba74845
MohamedMoustir <itsmoustir@gmail.com>
2025-11-21 21:28:26 +0100

    fix: Final security configuration clean-up and bean registration completion

--- src/main/java/com/example/logistique/config/SecurityConfig.java
+++ src/main/java/com/example/logistique/config/SecurityConfig.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable;
 
 @EnableWebSecurity
 @Configuration
@@ -24,7 +24,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf->disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->auth
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
