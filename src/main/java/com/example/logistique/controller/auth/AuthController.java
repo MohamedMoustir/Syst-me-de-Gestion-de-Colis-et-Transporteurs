cadee246a6d0fc99b3a06cfefd08301d881685e6
MohamedMoustir <itsmoustir@gmail.com>
2025-11-21 19:57:41 +0100

    fix: Correct AuthService injection in AuthController via constructor

--- src/main/java/com/example/logistique/controller/auth/AuthController.java
+++ src/main/java/com/example/logistique/controller/auth/AuthController.java
@@ -1,24 +1,24 @@
 package com.example.logistique.controller.auth;
 
-
 import com.example.logistique.dto.SignupRequest;
 import com.example.logistique.service.impl.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private  AuthService authService;
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupRequest request){
         String token = authService.signup(request);
         return ResponseEntity.ok("JWT Token: " + token);
-
     }
 
     @PostMapping("/login")
