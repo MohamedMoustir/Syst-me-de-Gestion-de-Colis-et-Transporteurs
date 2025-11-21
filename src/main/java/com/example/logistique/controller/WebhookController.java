94aba59d518c17afe17b3b2d6ae6d1758bf0ae5f
MohamedMoustir <itsmoustir@gmail.com>
2025-11-21 10:48:28 +0100

    test webhooks

--- src/main/java/com/example/logistique/controller/WebhookController.java
+++ src/main/java/com/example/logistique/controller/WebhookController.java
@@ -10,7 +10,6 @@ public class WebhookController {
 
     @PostMapping("/github-webhook")
     public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
-        System.out.println("\n===== WEBHOOK RECEIVED =====");
         return ResponseEntity.ok("OK");
     }
 }
