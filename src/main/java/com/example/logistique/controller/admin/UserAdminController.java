f4c425a71d88ed16e88f9fec437efb0a48df30e3
MohamedMoustir <itsmoustir@gmail.com>
2025-11-22 13:00:51 +0100

    fix error 403  role admin

--- src/main/java/com/example/logistique/controller/admin/UserAdminController.java
+++ src/main/java/com/example/logistique/controller/admin/UserAdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/admin")
 public class UserAdminController {
 
     private UserServiceImpl userService;
